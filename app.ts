b4793cb88ffe65515422e30bb21ec95fc69b0de7
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-01-30 09:53:13 +0100

    create a node server

---
+++ app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { Application } from 'express';
+import bodyParser from 'body-parser';
+import user from 'api/user';
+
+const app: Application = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api/user', user);
+
+export default app;
